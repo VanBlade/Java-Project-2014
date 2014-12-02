12d35e673a1a3ce5651569a07c401c493b35e52d
VanBlade <aidanvanbladel@gmail.com>
2014-12-02 11:43:10 +0000

    Game Instance
    game UI made

---
+++ PlayerGame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class PlayerGame extends JFrame{
+	
+	public PlayerGame(){
+		
+		setSize(200,200);
+		setResizable(false);
+        setLocation(250,200);
+        setTitle("Game Start");
+        
+		
+	}
+	
+}
--- slapsGame.java
+++ slapsGame.java
@@ -7,11 +7,10 @@ public class slapsGame extends JFrame implements ActionListener{
 	public static void main (String arg[]){
 	 slapsGame menu = new slapsGame();
 	 menu.setVisible(true);
-	 
+
 	}// ends main 
 	public slapsGame(){
 		Container cPane;
-		
 		
 		setSize(500,500);
 		setTitle("Main Menu");
@@ -24,23 +23,42 @@ public class slapsGame extends JFrame implements ActionListener{
         
          JButton   game= new JButton("Start Game");
         game.setBounds(200,250,120,40);
-	   //button.addActionListener(this);
+	   game.addActionListener(this);
         cPane.add(game);
 		
 		 JButton   highScores = new JButton("High Scores");
         highScores.setBounds(200,300,120,40);
-	   //highScores.addActionListener(this);
+	   highScores.addActionListener(this);
         cPane.add(highScores);
 		
 		 JButton   quit= new JButton("Quit");
         quit.setBounds(200,350,120,40);
-	   //quit.addActionListener(this);
+	   quit.addActionListener(this);
         cPane.add(quit);
+        JLabel gameTitle= new JLabel();
 		
 	}// ends slapsGame Method
 	 
-	public actionPerformed(ActionEvent e ){
+	public void actionPerformed(ActionEvent e ){
+	
+	String  buttonName;
+    buttonName = e.getActionCommand();
+    
+    if (buttonName.equals("Quit")) {
+           System.exit(0);
+    }else if(buttonName.equals("High Scores" )){
+    	displayHighScore();	
+    }else if (buttonName.equals("Start Game")){
+    playGame();
+    }// ends if else statements
+		
+	}// ends actionPerformed method
+	public void displayHighScore()
+	{
 		
 	}
-	
+	public void playGame(){
+		PlayerGame game = new PlayerGame();
+    	game.setVisible(true);
+	}
 }// ends slapsGame class
